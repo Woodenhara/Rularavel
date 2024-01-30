8937133ea18782ef15362e9e2933c1b8e284327d
Woodenhara <althafnova7@gmail.com>
2024-01-30 19:48:37 +0700

    Blade templating, minus yield

---
+++ resources/views/template/partials/sidebar.blade.php
@@ -0,0 +1,42 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+  <span class="align-middle">AdminKit</span>
+</a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="index.html">
+      <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+    </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-profile.html">
+      <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+    </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-in.html">
+      <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+    </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-up.html">
+      <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+    </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-blank.html">
+      <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+    </a>
+            </li>
+    </div>
+</nav>
