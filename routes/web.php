8ba3c0078642466a77728f53cfd4709a84348f16
Woodenhara <althafnova7@gmail.com>
2024-02-21 09:04:11 +0700

    Updating URI and Author

--- routes/web.php
+++ routes/web.php
@@ -33,18 +33,28 @@ Route::get('/tes', function() {
     return view('spp.sppc.create');
 });
 
-Route::controller(SppController::class)->group(function() {
-    Route::get('/spp', 'index')->name('spp.sppc.index');
-    Route::post('/spp', 'store')->name('spp.sppc.store');
-    Route::get('/spp/create', 'create')->name('spp.sppc.create');
-    Route::get('/spp/{id}','show')->name('spp.sppc.show');
-    Route::put('/spp/{id}','update')->name('spp.sppc.update');
-    Route::delete('/spp/{id}','destroy')->name('spp.sppc.destroy');
-    Route::get('/spp/{id}/edit','edit')->name('spp.sppc.edit');
+
+Route::middleware(['can:isUser'])->group(function() {
+    Route::get('/pembayaran', function() {
+        return view('spp.pembayaran.index');
+    })->name('pembayaran');
 });
 
-Route::resource('/kelass', KelassController::class);
-Route::resource('/user', UserController::class);
+Route::middleware(['can:isAdmin'])->group(function() {
+    Route::controller(SppController::class)->group(function() {
+        Route::get('/spp', 'index')->name('spp.sppc.index');
+        Route::post('/spp', 'store')->name('spp.sppc.store');
+        Route::get('/spp/create', 'create')->name('spp.sppc.create');
+        Route::get('/spp/{id}','show')->name('spp.sppc.show');
+        Route::put('/spp/{id}','update')->name('spp.sppc.update');
+        Route::delete('/spp/{id}','destroy')->name('spp.sppc.destroy');
+        Route::get('/spp/{id}/edit','edit')->name('spp.sppc.edit');
+    });
+
+    Route::resource('/kelass', KelassController::class);
+    Route::resource('/user', UserController::class);
+});
+
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware(['auth']);
 
@@ -53,6 +63,3 @@ Route::middleware('guest')->group(function() {
     Route::post('/login/proses', [AuthController::class, 'authenticate'])->name('auth.proses');
 });
 
-Route::get('/pembayaran', function() {
-    return view('spp.pembayaran.index');
-})->name('pembayaran')->middleware('auth');
